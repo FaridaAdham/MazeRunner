246af62328b476861ed4235220f5baf3fab4e957
FaridaAdham <farida_adham5595@hotmail.com>
2017-03-01 22:25:42 +0200

    Add files via upload

---
+++ src/mazerunner/Pmonster.java
@@ -0,0 +1,16 @@
+
+package mazerunner;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Farida Adham
+ */
+public class Pmonster {
+    public int random_i;
+    public int random_j;
+    public int monsterx;
+    public int monstery;
+    public boolean isdied = false;
+}
